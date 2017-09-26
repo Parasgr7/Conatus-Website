e1ed6594641fd6c95b4b55f534c7a911643b6eae
akshay <akshaya.a013@gmail.com>
2017-09-26 18:38:25 +0530

    setting.py

--- conatus/settings.py
+++ conatus/settings.py
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/home/Conatus/ConatusWebsite/conatusweb/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/Conatus/ConatusWebsite/conatusweb/static/'
